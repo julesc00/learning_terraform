ec5aece7dafdd7a3c4faab6d5558e502819338dd
Julio Briones <Julio_Briones@epam.com>
2022-03-10 20:07:59 -0600

    Added ingress,egress and tags for default vpc.

--- prod.tf
+++ prod.tf
@@ -8,3 +8,33 @@ resource "aws_s3_bucket" "prod_tf_course" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    # http
+    from_port     = 80
+    protocol      = "tcp"
+    to_port       = 80
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+  ingress {
+    # https
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"  # Allow all
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
