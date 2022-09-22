853753cb95b4f30dfc4cc726b4296465757fc31e
Julio Briones <Julio_Briones@epam.com>
2022-09-22 13:50:55 -0500

    Addded s3 Terraform remote state example.

--- s3_remote_state_example.tf
+++ s3_remote_state_example.tf
@@ -1,3 +1,23 @@
+/*
+In order to first create the s3 bucket for the remote state, we have to first provision this s3
+bucking into AWS by run `terraform apply` from our local state, then uncomment the terraform backend
+section and then rerun `terraform apply` so the state can find such bucket in AWS S3 service.
+
+To successfully destroy this infrastructure, we have to follow a two-step process.
+1. We have to delete/comment the terraform backend section.
+2. Run `terraform init` again to recreate the local state.
+3. Then run `terraform destroy`
+*/
+terraform {
+  backend "s3" {
+    bucket = aws_s3_bucket.terraform_state_example.bucket
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = aws_dynamodb_table.terraform_locks.name
+    encrypt = true
+  }
+}
+
 resource "aws_s3_bucket" "terraform_state_example" {
   bucket = "terraform_state_julio"
 
@@ -19,5 +39,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+// As a locking mechanism to allow only 1 user to write to state
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform_state_locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
